7df44e5d893ef9a3b2c92e77e2ec3c5199bdbffe
sam <abluecar@gmail.com>
2024-08-24 23:08:49 -0400

    chore: Check that fetching from api works

--- src/index.js
+++ src/index.js
@@ -1,2 +1,31 @@
-console.log("hello from index.js");
-console.log("hello again");
+import "./style.css";
+
+// TODO: Add error checking
+// Try one of these solutions: https://stackoverflow.com/questions/54163952/async-await-in-fetch-how-to-handle-errors
+
+async function fetchData(query) {
+  // "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?key=4JADYSQFJBLV5TNHBEPMXVB9Y";
+  const request = new Request(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${query}?key=4JADYSQFJBLV5TNHBEPMXVB9Y`,
+  );
+
+  try {
+    const response = await fetch(request, { mode: "cors" });
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    return json;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+const inputCity = document.getElementById("input-city");
+const buttonCity = document.getElementById("button-city");
+buttonCity.addEventListener("click", (e) => {
+  e.preventDefault();
+  const city = inputCity.value;
+  fetchData(city).then((value) => console.log(value));
+});
