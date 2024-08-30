35b6dd866603a620668c1d6e80284434c14b23c2
Kwok Lau <kslau07@protonmail.com>
2024-08-30 16:10:09 -0400

    style: Add header with logo

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
-import "./style.css";
+import './style.css';
+import Logo from './images/logo.png';
+
+const logo = document.querySelector('.logo');
+logo.src = Logo;
+logo.alt = 'Logo with a cloud, sun, and rain.';
 
 // TODO: Add error checking
 // Try one of these solutions: https://stackoverflow.com/questions/54163952/async-await-in-fetch-how-to-handle-errors
 
-async function fetchData(query) {
+async function fetchWeather(city) {
   // "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?key=4JADYSQFJBLV5TNHBEPMXVB9Y";
   const request = new Request(
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${query}?key=4JADYSQFJBLV5TNHBEPMXVB9Y`,
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=4JADYSQFJBLV5TNHBEPMXVB9Y`,
   );
 
   try {
-    const response = await fetch(request, { mode: "cors" });
+    const response = await fetch(request, { mode: 'cors' });
     if (!response.ok) {
       throw new Error(`Response status: ${response.status}`);
     }
@@ -22,18 +27,28 @@ async function fetchData(query) {
   }
 }
 
-const inputCity = document.getElementById("input-city");
-const buttonCity = document.getElementById("button-city");
-const display = document.querySelector(".display");
+const inputCity = document.getElementById('input-city');
+const buttonCity = document.getElementById('button-city');
+const display = document.querySelector('.display');
 
-let london;
-
-buttonCity.addEventListener("click", (e) => {
+buttonCity.addEventListener('click', (e) => {
   e.preventDefault();
 
   const city = inputCity.value;
-  fetchData(city).then((data) => {
-    console.log(data.currentConditions);
+  fetchWeather(city).then((data) => {
+    const {
+      currentConditions: {
+        conditions: cond,
+        temp,
+        humidity,
+        precip,
+        precipprob,
+      },
+    } = data;
+    // console.log(cond, temp, humidity, precip, precipprob);
+    // display.textContent = `${cond} ${temp} ${humidity} ${precip} ${precipprob}}`;
+    console.log(data);
+    // console.log(data.currentConditions);
     // display.textContent = json;
   });
 });
